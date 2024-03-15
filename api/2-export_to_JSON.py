522c6ac4877d7cf8776e84180e7065e6853025df
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-15 18:24:00 +0000

    updated task2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,36 +1,51 @@
 #!/usr/bin/python3
 """
-Module documentation for task 2 about retrieve json
+Export data from an API endpoint to JSON format.
 """
 
-from sys import argv
-from json import dump
-from requests import get
+import sys
+import json
+import requests
 
-url_base = 'https://jsonplaceholder.typicode.com/users/'
+BASE_URL = 'https://jsonplaceholder.typicode.com/users/'
 
+def export_to_json(user_id):
+    """
+    Retrieve tasks owned by a user from the API and export to JSON.
 
-def export_json(id):
-    """ function that retrieve data in format json """
+    Args:
+        user_id (int): The ID of the user whose tasks will be retrieved.
+    """
+    user_url = f"{BASE_URL}{user_id}"
+    todos_url = f"{BASE_URL}{user_id}/todos/"
 
-    file_name = str(id) + '.json'
-    usr = get(url_base + str(id)).json()
-    todos = get(url_base + str(id) + '/todos/').json()
-    items_data = []
-    retrieve_json = dict()
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
 
-    for todo in todos:
-        item_dict = dict()
-        item_dict['task'] = todo['title']
-        item_dict['completed'] = todo['completed']
-        item_dict['username'] = usr['username']
-        items_data.append(item_dict)
+    if user_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error: Failed to retrieve data from the API.")
+        return
 
-    retrieve_json[str(id)] = items_data
+    user_data = user_response.json()
+    todos_data = todos_response.json()
 
-    with open(file_name, 'w', encoding='utf-8') as file_json:
-        dump(retrieve_json, file_json)
+    tasks = []
+    for todo in todos_data:
+        task = {
+            "task": todo["title"],
+            "completed": todo["completed"],
+            "username": user_data["username"]
+        }
+        tasks.append(task)
 
+    output_filename = f"{user_id}.json"
+    with open(output_filename, 'w') as file:
+        json.dump({str(user_id): tasks}, file, indent=4)
 
-if __name__ == '__main__':
-    export_json(int(argv[1]))
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 export_to_json.py <user_id>")
+        sys.exit(1)
+
+    user_id = int(sys.argv[1])
+    export_to_json(user_id)
