3efb6fc55e42093549cf05a03dbbbd0e558f4bc0
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-15 17:00:14 +0000

    updated task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,30 +2,26 @@
 """
 Module: export_to_CSV
 
-This module provides functionality to
-export task data to a CSV file based on a given user ID.
+This module provides functionality to export task data to a CSV file based on a given user ID.
 """
 
 import csv
 import json
+import os
 import sys
-import os  # Import os module for file path operations
 
 
 def export_to_csv(user_id):
     """
-    Export tasks belonging to the
-    specified user to a CSV file.
+    Export tasks belonging to the specified user to a CSV file.
 
     Args:
-        user_id (int): The ID of the user
-        whose tasks are to be exported.
+        user_id (int): The ID of the user whose tasks are to be exported.
 
     Returns:
         None
     """
-    # Ensure that todos.json file exists
-    # in the current directory or provide correct path
+    # Ensure that todos.json file exists in the current directory or provide correct path
     if not os.path.exists('todos.json'):
         print("Error: todos.json file not found.")
         sys.exit(1)
@@ -41,20 +37,14 @@ def export_to_csv(user_id):
 
     csv_filename = f"{user_id}.csv"
     with open(csv_filename, 'w', newline='') as csv_file:
-        fieldnames = [
-            'USER_ID',
-            'USERNAME',
-            'TASK_COMPLETED_STATUS',
-            'TASK_TITLE'
-            ]
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
         writer.writeheader()
         for task in tasks:
             writer.writerow({
                 'USER_ID': task['userId'],
-                # Assuming username is same for all tasks
-                'USERNAME': data[0]['username'],
+                'USERNAME': data[0]['username'],  # Assuming username is same for all tasks
                 'TASK_COMPLETED_STATUS': str(task['completed']),
                 'TASK_TITLE': task['title']
             })
