596f1b43e48266effad459450caec0d587165c05
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-15 17:06:52 +0000

    updated task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,59 +1,34 @@
 #!/usr/bin/python3
-"""
-Module: export_to_CSV
-
-This module provides functionality to export task data to a CSV file based on a given user ID.
-"""
-
+"""Script to use a REST API for a given employee ID, returns
+information about his/her TODO list progress and export in CSV"""
 import csv
-import json
-import os
+import requests
 import sys
-
-
-def export_to_csv(user_id):
-    """
-    Export tasks belonging to the specified user to a CSV file.
-
-    Args:
-        user_id (int): The ID of the user whose tasks are to be exported.
-
-    Returns:
-        None
-    """
-    # Ensure that todos.json file exists in the current directory or provide correct path
-    if not os.path.exists('todos.json'):
-        print("Error: todos.json file not found.")
-        sys.exit(1)
-
-    with open('todos.json') as json_file:
-        data = json.load(json_file)
-
-    # Check if there are tasks associated with the specified user ID
-    tasks = [task for task in data if task['userId'] == user_id]
-    if not tasks:
-        print(f"No tasks found for user ID {user_id}.")
-        sys.exit(1)
-
-    csv_filename = f"{user_id}.csv"
-    with open(csv_filename, 'w', newline='') as csv_file:
-        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for task in tasks:
-            writer.writerow({
-                'USER_ID': task['userId'],
-                'USERNAME': data[0]['username'],  # Assuming username is same for all tasks
-                'TASK_COMPLETED_STATUS': str(task['completed']),
-                'TASK_TITLE': task['title']
-            })
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <user_id>")
+        print(f"UsageError: python3 {__file__} employee_id(int)")
         sys.exit(1)
 
-    user_id = sys.argv[1]
-    export_to_csv(int(user_id))
+    API_URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
+
+    response = requests.get(
+        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
+        params={"_expand": "user"}
+    )
+    data = response.json()
+
+    if not len(data):
+        print("RequestError:", 404)
+        sys.exit(1)
+
+    username = data[0]["user"]["username"]
+
+    with open(f"{EMPLOYEE_ID}.csv", "w", newline="") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
+        for task in data:
+            writer.writerow(
+                [EMPLOYEE_ID, username, str(task["completed"]), task["title"]]
+            )
